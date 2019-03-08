0354e12afe943f2704cadbf53377297309c35647
Abdul Samad <abdulsamad.official@gmail.com>
2019-03-08 08:54:36 +0500

    IC Design Uploaded

---
+++ js/sam.js
@@ -0,0 +1,94 @@
+/*
+perfData = window.performance.timing, // The PerformanceTiming interface
+EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart), // Calculated Estimated Time of Page Load which returns negative value.
+pageLoadTime = parseInt((EstimatedTime/1000)%60)*100;
+//window.alert(time);
+*/
+
+(function($) {
+$.fn.menumaker = function(options) {  
+ var cssmenu = $(this), settings = $.extend({
+   format: "dropdown",
+   sticky: false
+ }, options);
+ return this.each(function() {
+   $(this).find(".button").on('click', function(){
+     $(this).toggleClass('menu-opened');
+     var mainmenu = $(this).next('ul');
+     if (mainmenu.hasClass('open')) { 
+       mainmenu.slideToggle().removeClass('open');
+     }
+     else {
+       mainmenu.slideToggle().addClass('open');
+       if (settings.format === "dropdown") {
+         mainmenu.find('ul').show();
+       }
+     }
+   });
+   cssmenu.find('li ul').parent().addClass('has-sub');
+multiTg = function() {
+     cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+     cssmenu.find('.submenu-button').on('click', function() {
+       $(this).toggleClass('submenu-opened');
+       if ($(this).siblings('ul').hasClass('open')) {
+         $(this).siblings('ul').removeClass('open').slideToggle();
+       }
+       else {
+         $(this).siblings('ul').addClass('open').slideToggle();
+       }
+     });
+   };
+   if (settings.format === 'multitoggle') multiTg();
+   else cssmenu.addClass('dropdown');
+   if (settings.sticky === true) cssmenu.css('position', 'fixed');
+resizeFix = function() {
+  var mediasize = 1000;
+     if ($( window ).width() > mediasize) {
+       cssmenu.find('ul').show();
+     }
+     if ($(window).width() <= mediasize) {
+       cssmenu.find('ul').hide().removeClass('open');
+     }
+   };
+   resizeFix();
+   return $(window).on('resize', resizeFix);
+ });
+  };
+})(jQuery);
+
+(function($){
+$(document).ready(function(){
+$("#cssmenu").menumaker({
+   format: "multitoggle"
+});
+});
+})(jQuery);
+
+
+
+
+(function($){
+  $(window).load(function(){
+    
+    $("#content-1").mThumbnailScroller({
+      callbacks:{
+        onInit:function(){
+          var $this=$(this);
+          $("#si").text("true");
+          setTimeout(function(){ $this.addClass("scroller-ready"); },300); //optional timeout
+        }
+      }
+    });
+    
+  });
+})(jQuery);
+
+
+
+$(document).ready(function() {
+
+setTimeout(function(){
+$('body').addClass('loaded');
+}, 10);
+  
+});
